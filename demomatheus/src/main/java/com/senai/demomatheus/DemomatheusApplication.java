b38ecd2116b5e5be886aed73708181a2fb325da3
Ademario Junior <ademariofernandesramos@gmail.com>
2021-12-18 22:15:01 -0300

    Add gitignore for any files java and application java core

---
+++ demomatheus/src/main/java/com/senai/demomatheus/DemomatheusApplication.java
@@ -0,0 +1,13 @@
+package com.senai.demomatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomatheusApplication.class, args);
+	}
+
+}
